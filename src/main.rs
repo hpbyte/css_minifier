e7cce7c95e638c1d5a8444f173bb6906c6d541ea
Htoo Pyae <htoopyae.dev@gmail.com>
2023-01-05 22:58:14 +0700

    feat: added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Path of the input file
+    #[arg(short, long)]
+    input_file: String,
+
+    // Path of the output file
+    #[arg(short, long)]
+    output_file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("in {}", args.input_file);
+    println!("out {}", args.output_file);
 }
