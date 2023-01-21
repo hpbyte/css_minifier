556f2638c83f5d2c20bff5608ead031390dd2c2d
Htoo Pyae <htoopyae.dev@gmail.com>
2023-01-21 11:15:43 +0700

    feat: logic updated

--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,78 @@ fn main() -> std::io::Result<()> {
     let mut fo =
         fs::File::create(&args.output_file).expect("error encountered while creating the file!");
 
-    println!("in {}", args.input_file);
-    println!("out {}", args.output_file);
+    let mut is_comment = false;
+    let mut is_space = false;
 
     for line in fi.lines() {
-        let mut out_str = String::from("");
+        let in_line_chars = line.chars().collect::<Vec<char>>();
+        let mut out_line_chars: Vec<char> = vec![];
+
+        if in_line_chars.len() == 0 {
+            continue;
+        }
+
+        let mut o = 0;
 
         // remove comments
+        for i in 0..in_line_chars.len() - 1 {
+            // comment ends
+            if is_comment && in_line_chars[i] == '*' && in_line_chars[i + 1] == '/' {
+                is_comment = false;
+                continue;
+            }
+            if is_comment {
+                continue;
+            }
+            // comment starts
+            if !is_comment && in_line_chars[i] == '/' && in_line_chars[i + 1] == '*' {
+                is_space = false;
+                is_comment = true;
+                continue;
+            }
 
-        // remove spaces
-        out_str.push_str(line.replace(" ", "").as_str());
+            // remove spaces
+            if in_line_chars[i] != ' ' && in_line_chars[i] != '\t' {
+                is_space = false;
+            }
+            if in_line_chars[i] == '\r'
+                || in_line_chars[i] == '\n'
+                || (is_space && (in_line_chars[i] == ' ' || in_line_chars[i] == '\t'))
+            {
+                is_space = true;
+                continue;
+            }
+            if in_line_chars[i] == '{' {
+                loop {
+                    if (o > 1) && (in_line_chars[o - 1] == ' ' || in_line_chars[o - 1] == '\t') {
+                        break;
+                    }
+                    o -= 1;
+                }
+            }
+            if in_line_chars[i] == '>' {
+                loop {
+                    if (o > 1) && (in_line_chars[o - 1] == ' ' || in_line_chars[o - 1] == '\t') {
+                        break;
+                    }
+                    o -= 1;
+                }
+            }
+            if in_line_chars[i] == ':'
+                || in_line_chars[i] == ';'
+                || in_line_chars[i] == ','
+                || in_line_chars[i] == '>'
+            {
+                is_space = true;
+            }
+
+            // copy
+            out_line_chars.push(in_line_chars[i]);
+            o += 1;
+        }
 
         // write
-        fo.write(out_str.as_bytes())?;
+        fo.write(String::from_iter(out_line_chars).as_bytes())?;
     }
 
     Ok(())
