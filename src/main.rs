6c71bb54d62a67350cc289d8c1414c12877a803e
Htoo Pyae <htoopyae.dev@gmail.com>
2023-01-21 11:15:43 +0700

    started

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::fs;
+use std::io::prelude::*;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -12,9 +14,27 @@ struct Args {
     output_file: String,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Args::parse();
+
+    let fi = fs::read_to_string(&args.input_file).expect("could not read input file");
+    let mut fo =
+        fs::File::create(&args.output_file).expect("error encountered while creating the file!");
 
     println!("in {}", args.input_file);
     println!("out {}", args.output_file);
+
+    for line in fi.lines() {
+        let mut out_str = String::from("");
+
+        // remove comments
+
+        // remove spaces
+        out_str.push_str(line.replace(" ", "").as_str());
+
+        // write
+        fo.write(out_str.as_bytes())?;
+    }
+
+    Ok(())
 }
